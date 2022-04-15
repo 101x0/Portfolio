d02c13aab7ce930df52f89095efd1aa87c46b81d
101x0 <sergio.de.diego.ter@gmail.com>
2022-04-15 12:24:21 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let cards = document.getElementsByClassName("card_container");
+
+cards[0].addEventListener("click", openTasks);
+cards[1].addEventListener("click", openCesar);
+cards[2].addEventListener("click", openChess);
+cards[3].addEventListener("click", openComments);
+
+
+function openTasks(){
+    window.open("https://101x0.github.io/Gestor_Tareas/", "", "width=500, height=600, top=50,left=400");
+}
+
+function openCesar(){
+    window.open("https://101x0.github.io/CifradoCesar/", "", "width=850, height=1000, top=50,left=400");
+}
+
+function openChess(){
+    window.open("https://101x0.github.io/Tablero_Ajedrez/", "", "width=700, height=650, top=50,left=400");
+}
+
+function openComments(){
+    window.open("https://101x0.github.io/Sistema_de_comentarios/", "", "width=700, height=650, top=50,left=400");
+}
