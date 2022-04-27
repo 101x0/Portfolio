3fb5d5bfd31f142e09de352aee30ff06dafd8fbb
101x0 <sergio.de.diego.ter@gmail.com>
2022-04-27 13:30:24 +0200

    add MineSweeper

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ cards[0].addEventListener("click", openTasks);
 cards[1].addEventListener("click", openCesar);
 cards[2].addEventListener("click", openChess);
 cards[3].addEventListener("click", openComments);
+cards[4].addEventListener("click", openMineSweeper);
 
 
 function openTasks(){
@@ -20,4 +21,8 @@ function openChess(){
 
 function openComments(){
     window.open("https://101x0.github.io/Sistema_de_comentarios/", "", "width=700, height=650, top=50,left=400");
+}
+
+function openMineSweeper(){
+    window.open("https://101x0.github.io/BuscaMinas/", "", "width=700, height=750, top=20,left=400");
 }
